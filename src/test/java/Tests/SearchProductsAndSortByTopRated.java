306c9abebf95aa3c90689f3d38c3a6a50fa9bfab
AndrejaAvakumovic <andreja.avakumovic@gmail.com>
2022-01-24 14:54:54 +0100

    git commit

--- src/test/java/Tests/SearchProductsAndSortByTopRated.java
+++ src/test/java/Tests/SearchProductsAndSortByTopRated.java
@@ -3,11 +3,14 @@ package Tests;
 import Pages.HomePage;
 import Pages.NightCreams;
 import Pages.Products;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import zipkin2.Call;
 
 public class SearchProductsAndSortByTopRated extends BaseTest {
+    //This test should search and sort products by top rated.
+
     @Test
     public void searchAndSortProducts(){
         ChromeDriver driver = new ChromeDriver();
@@ -17,6 +20,7 @@ public class SearchProductsAndSortByTopRated extends BaseTest {
         homePage.cancelSignIn(driver);
         NightCreams nightCreams = new NightCreams(driver);
         nightCreams.scrollOnTopRated();
+        Assert.assertTrue("Products are not sorted by top rated order",  nightCreams.isElementPresent(nightCreams.topratedSelectedScroll));
         driver.quit();
     }
 }
